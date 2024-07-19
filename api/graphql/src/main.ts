48155f9134cf60ee0be26eb6b26a943b8cf447a5
Anthony N. Rivera Vega <anthonyrivera51@gmail.com>
2024-07-18 22:00:35 -0500

    ability cors

--- api/graphql/src/main.ts
+++ api/graphql/src/main.ts
@@ -3,10 +3,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
 import * as process from 'process';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
+  
+  // Define CORS options
+  const corsOptions: CorsOptions = {
+    origin: ['http://localhost:3002', 'https://your-nextjs-domain.com'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // Enable CORS credentials (cookies, authorization headers, etc.)
+  };
+  
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe());
   app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }));
   const PORT = process.env.PORT || 4000;
--- api/rest/src/main.ts
+++ api/rest/src/main.ts
@@ -2,9 +2,20 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  // Define CORS options
+  const corsOptions: CorsOptions = {
+    origin: ['http://localhost:3002', 'https://tenderos-frontend-umohnyjl4q-ue.a.run.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // Enable CORS credentials (cookies, authorization headers, etc.)
+  };
+  
+  app.enableCors(corsOptions);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
